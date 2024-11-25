5d396f0d69ae828d7d90bdcebeea5cf977a53b2c
jayelrey7 <115120675+jayelrey7@users.noreply.github.com>
2024-11-24 21:18:16 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,8 @@ game 'gta5'
 
 lua54 'yes'
 
-author 'Maji#1118'
-description 'Fuel Delivery Job'
+author 'jayelrey'
+description 'Oil Refinery Job'
 version '1.0.0'
 
 shared_scripts {
@@ -13,8 +13,9 @@ shared_scripts {
     '@PolyZone/BoxZone.lua'
 }
 client_scripts {
-    'client/main.lua',
+    'client/main.lua'
 }
+
 server_script 'server/main.lua'
 
 dependencies {
