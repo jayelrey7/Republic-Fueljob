6a3c57a97a5f1f4a9c04e93078a2979698c9cf8a
jayelrey7 <115120675+jayelrey7@users.noreply.github.com>
2024-11-24 21:17:29 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,88 +1,59 @@
 Config = {}
 
+-- Debug Settings
 Config.Debug = false
 
+-- Location Settings
 Config.Blip = {
     {
-    title = "Fuel Delivery",
-    color = 12,
-    id = 477,
-    x = 1721.87,
-    y = -1557.67,
-    z = 112.65,
+        title = "Oil Refinery",
+        color = 12,
+        id = 477,
+        x = 598.27,
+        y = 2930.26,
+        z = 40.91,
     },
 }
 
-Config.Target = 'qb' -- REMOVED SUPPORT FOR OX
-
-Config.UseMenu = false --- Set this to "true" if you like to use the menu to interact with the Boss 
-
-Config.Menu = 'qb' -- REMOVED SUPPORT FOR OX
-
-Config.PedType = "a_m_m_ktown_01"
-
-Config.VehicleKeys = 'qb-vehiclekeys' -- qb-vehiclekeys or mk_vehiclekeys
-
-Config.TruckToSpawn = "packer" -- Vehicle MUST be able to pull a large trailer. 
-
-Config.TrailerToSpawn = "tanker2" -- Must be a tanker variant, or else the rope will not place in the right position. If this does not bother you, then change to your liking.
-
-Config.PayPerFueling = math.random(250, 500) -- This is multiplied by the amount of refuels the player does. So if they refuel 5 stations, they will get 5*math.random
-
-Config.FuelScript = 'LegacyFuel' -- cdn-fuel or ps-fuel or LegacyFuel
-
-Config.PayType = 'bank' -- Where does the money come from? bank or cash
-
-Config.TruckPrice = 5000 -- Cost of the vehicle to pull out //// //// if the player chose the 2nd option (Start Work With Your Own Truck) this is disabled and the "Config.TankPrice" will take place
-
-Config.TankPrice = 2000 --- Cost of the tank for the players that use their own trucks to do the job
-
-Config.MaxFuelDeliveries = 5 -- Maximum amount of deliveries before they need to return and refuel
-
-Config.PumpLocations = { -- WHEN ADDING NEW STATIONS, THE VECTOR3 MUST BE WHERE YOU WANT TO SPAWN THE REFUEL PROP
-    ["1"] = {
-        coords = vector3(-57.77, -1774.39, 29.03),
-    },
-    ["2"] = {
-        coords = vector3(-723.21, -909.07, 19.34),
-    },
-    ["3"] = {
-        coords = vector3(-1437.37, -259.18, 46.26),
-    },
-    ["4"] = {
-        coords = vector3(288.48, -1251.04, 29.44),
-    },
-    ["5"] = {
-        coords = vector3(-520.42, -1225.82, 18.45),
-    },
-    ["6"] = {
-        coords = vector3(-2567.13, 2317.27, 33.22),
-    },
-    ["7"] = {
-        coords = vector3(272.56, 2598.52, 44.58),
-    },
-    ["8"] = {
-        coords = vector3(1203.96, 2642.59, 37.84),
-    },
-    ["9"] = {
-        coords = vector3(1778.86, 3320.2, 41.42),
-    },
-    ["10"] = {
-        coords = vector3(1988.74, 3781.08, 32.18),
-    },
-    ["11"] = {
-        coords = vector3(2670.09, 3259.69, 55.24),
-    },
-    ["12"] = {
-        coords = vector3(1695.46, 6428.0, 32.63),
-    },
-    ["13"] = {
-        coords = vector3(-91.0, 6396.47, 31.64),
-    },
-    ["14"] = {
-    coords = vector3(-1830.18, 781.39, 138.33),
-    },
-    ["15"] = {
-    coords = vector3(-992.14, -3034.84, 13.95),
-    },
+Config.OilPumps = {
+    {coords = vector4(694.04, 2884.62, 50.13, 328.71)},
+    {coords = vector4(608.11, 2854.51, 39.99, 351.49)},
+    {coords = vector4(654.07, 2924.03, 42.1, 130.7)},
+    {coords = vector4(650.11, 3015.86, 43.32, 203.48)},
+    {coords = vector4(600.75, 3018.11, 41.95, 61.22)},
+    {coords = vector4(579.65, 2925.3, 40.89, 353.68)},
+    {coords = vector4(544.72, 2875.93, 43.06, 61.51)},
+    {coords = vector4(494.89, 2961.67, 42.28, 262.37)}
 }
+
+Config.Refinery = vector4(2675.44, 1491.88, 24.5, 102.43)
+Config.ReturnLocation = vector4(617.96, 2928.02, 40.19, 3.63)
+Config.OilCollection = vector4(1686.17, -1457.77, 112.39, 254.5)
+
+-- Framework Settings
+Config.Target = 'qb'
+Config.UseMenu = false
+Config.Menu = 'qb'
+Config.VehicleKeys = 'qb-vehiclekeys'
+Config.FuelScript = 'LegacyFuel'
+
+-- Vehicle Settings
+Config.PedType = "s_m_m_dockwork_01"  -- Changed to dock worker model
+Config.TruckToSpawn = "packer"
+Config.TrailerToSpawn = "tanker2"
+Config.OilTankerModel = 'tanker'
+
+-- Payment Settings
+Config.PayType = 'bank'
+Config.TankPrice = 2000
+Config.OilJobPay = 1250
+Config.OilTruckPrice = 1500
+Config.OilCollectionPay = 500
+
+-- Added Missing Required Configs
+Config.TruckPrice = 2500 -- Added missing truck rental price
+Config.PayPerDelivery = 750 -- Added missing delivery payment
+Config.PayPerFueling = 250 -- Added missing fueling payment
+Config.StartLocation = vector4(617.96, 2928.02, 40.19, 3.63) -- Added missing start location
+
+return Config
